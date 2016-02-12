64b95276abfd5ff0946e8ea85090956ee4506ec7
Thiago Ferreira <thiago.patricio@ccc.ufcg.edu.br>
2016-02-12 13:22:52 -0300

    First Commit - Scroll menu item

--- src/feature_menu_layer.c
+++ src/feature_menu_layer.c
@@ -12,6 +12,12 @@
 #define STORAGE_ITEMS 20
 #define STORAGE_ITEM_COUNT 0
 
+// For text scrolling
+#define SCROLL_MENU_ITEM_WAIT_TIMER 1000
+#define SCROLL_MENU_ITEM_TIMER 400
+#define MENU_CHARS_VISIBLE 16
+#define SUBTITLE_CHARS_VISIBLE 22
+
 // Pebble KEY
 #define KEY_RESULT        1
 
@@ -49,6 +55,16 @@ add_item(char *name, char *description)
     num_items++;    
   }
 }
+
+typedef struct WindowData WindowData;
+struct WindowData {
+  AppTimer *menu_scroll_timer;
+  MenuLayer *menu;
+  int   menu_scroll_offset;
+  bool  moving_forwards_in_menu;
+	bool	scrolling_still_required;
+  bool	menu_reloading_to_scroll;
+};
 
 char list_text[MAX_TEXT_SIZE];
 
@@ -277,6 +293,98 @@ static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, v
 
 }
 
+void get_menu_text(WindowData* window_data, int index, char** text, char** subtext) {
+//   MenuItem* menu_item = getMenuItem(window_data, index);
+//   *text = menu_item ? menu_item->text : NULL;
+//   *subtext = menu_item && menu_item->flags & ITEM_FLAG_TWO_LINER ? 
+//                          menu_item->text + strlen(menu_item->text) + 1 : NULL;
+//   if(*subtext != NULL && strlen(*subtext) == 0) {
+//     *subtext = NULL;
+//   }
+  
+  *text = list_items[index].name;
+  APP_LOG(APP_LOG_LEVEL_INFO, "Get menu text" );
+  MenuIndex menuIndex = menu_layer_get_selected_index(window_data->menu);
+  if(*text && menuIndex.row == index) {
+    int len = strlen(*text);
+    if(len - MENU_CHARS_VISIBLE - window_data->menu_scroll_offset > 0) {
+      *text += window_data->menu_scroll_offset;
+      window_data->scrolling_still_required = true;
+    }
+  }
+}
+
+static void scroll_menu_callback(void* data) {
+ APP_LOG(APP_LOG_LEVEL_DEBUG, "SCROLL MENU CALLBACK timer");
+  WindowData* window_data = (WindowData*)data;
+  if(!window_data->menu) {
+    return;
+  }
+  window_data->menu_scroll_timer = NULL;
+  window_data->menu_scroll_offset++;
+  if(!window_data->scrolling_still_required) {
+    return;
+  }
+
+    // Redraw the menu with this scroll offset
+  MenuIndex menuIndex = menu_layer_get_selected_index(window_data->menu);
+  if(menuIndex.row != 0) {
+    window_data->menu_reloading_to_scroll = true;
+  }
+  window_data->scrolling_still_required = false;
+  menu_layer_reload_data(window_data->menu);
+  window_data->menu_scroll_timer = app_timer_register(SCROLL_MENU_ITEM_TIMER, scroll_menu_callback, 
+                                                      window_data);
+}
+
+static void initiate_menu_scroll_timer(WindowData* window_data) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Initiate Menu Scroll Timer");
+  // If there is already a timer then reschedule it, otherwise create one
+  bool need_to_create_timer = true;
+  window_data->scrolling_still_required = true;
+  window_data->menu_scroll_offset = 0;
+  window_data->menu_reloading_to_scroll = false;
+  if(window_data->menu_scroll_timer) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Rescheduling timer");
+    need_to_create_timer = !app_timer_reschedule(window_data->menu_scroll_timer, 
+                                                SCROLL_MENU_ITEM_WAIT_TIMER);
+  }
+  if(need_to_create_timer) {
+     APP_LOG(APP_LOG_LEVEL_DEBUG, "Creating timer");
+    window_data->menu_scroll_timer = app_timer_register(SCROLL_MENU_ITEM_WAIT_TIMER, 
+                                                        scroll_menu_callback, window_data);
+  }
+}
+
+static void selection_changed_callback(MenuLayer *menu_layer, MenuIndex new_index, MenuIndex old_index, 
+                                       void *data) {
+  WindowData* window_data = (WindowData*)data;
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Selection Changed Callback");
+  
+  if (window_data) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "IS NULL");
+ 
+  } else {
+      APP_LOG(APP_LOG_LEVEL_DEBUG, "NOT NULL");
+      window_data->menu_scroll_offset = 0;    
+  }
+  
+//   APP_LOG(APP_LOG_LEVEL_DEBUG, "Printing bool: %d\n", new_index.row >= old_index.row);
+
+    
+//   APP_LOG(APP_LOG_LEVEL_DEBUG, "Printing bool: %d\n", new_index.row >= old_index.row);
+
+//   window_data->moving_forwards_in_menu = false;
+
+//   if(!window_data->menu_reloading_to_scroll) {
+//     initiate_menu_scroll_timer(window_data);
+//   } else {
+//     window_data->menu_reloading_to_scroll = false;
+//   }
+}
+
+
+
 static void main_window_load(Window *window) {
   // Here we load the bitmap assets
   s_menu_icons[0] = gbitmap_create_with_resource(RESOURCE_ID_MENU_CHECKED_ITEM);
@@ -294,7 +402,8 @@ static void main_window_load(Window *window) {
     .get_header_height = menu_get_header_height_callback,
     .draw_header = menu_draw_header_callback,
     .draw_row = menu_draw_row_callback,
-    .select_click = menu_select_callback,
+    .selection_changed = selection_changed_callback,
+    .select_click = menu_select_callback
   });
 
   // Bind the menu layer's click config provider to the window for interactivity
@@ -338,8 +447,8 @@ static void deinit() {
 
 int main(void) {
   load_data();
-//   add_item("Cheese", "6 units");
-//   add_item("Milk", "6 units");
+  add_item("This is a really big text", "6 units");
+  add_item("This is an even bigger text", "6 units");
 //   add_item("Ham", "6 units");
 //   add_item("Krokitos", "2 caixas");
   if (num_items == 0) {
